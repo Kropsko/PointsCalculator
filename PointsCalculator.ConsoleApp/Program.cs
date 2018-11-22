15b48951a714243400f4a9e63cec6d6c697f61cb
WRO_NT1\pjur00 <Piotr.Jura@si-consulting.pl>
2018-11-22 13:14:11 +0100

    Dostosowanie aplikacji konsolowej do wprowadzonych poprzednio zmian.

--- PointsCalculator.ConsoleApp/Program.cs
+++ PointsCalculator.ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Autofac;
 using PointsCalculator.Container;
+using PointsCalculator.Domain;
 using PointsCalculator.Domain.Infrastructure;
 using System;
 
@@ -17,34 +18,35 @@ namespace PointsCalculator.ConsoleApp
             var configurationService = container.Resolve<IConfigurationService>();
             var actionService = container.Resolve<IActionService>();
 
-            var gameplay = gameplayService.GetCompleteGameplay(1); //gameplayService.CreateNewGameplay();
+            //var gameplay = gameplayService.GetCompleteGameplay(1); 
+            var gameplay = gameplayService.CreateNewGameplay();
 
-            //var playerOne = playerService.CreateNewPlayer("Gracz 1");
-            var playerOne = playerService.GetPlayer(1);
-            //var playerTwo = playerService.CreateNewPlayer("Gracz 2");
-            var playerTwo = playerService.GetPlayer(2);
-            //var playerThree = playerService.CreateNewPlayer("Gracz 3");
-            var playerThree = playerService.GetPlayer(3);
+            var playerOne = playerService.CreateNewPlayer("Gracz 1");
+            //var playerOne = playerService.GetPlayer(1);
+            var playerTwo = playerService.CreateNewPlayer("Gracz 2");
+            //var playerTwo = playerService.GetPlayer(2);
+            var playerThree = playerService.CreateNewPlayer("Gracz 3");
+            //var playerThree = playerService.GetPlayer(3);
 
-            //gameplayService.SetPlayerForGameplay(playerOne, gameplay);
-            //gameplayService.SetPlayerForGameplay(playerTwo, gameplay);
+            gameplayService.SetPlayerForGameplay(playerOne, gameplay);
+            gameplayService.SetPlayerForGameplay(playerTwo, gameplay);
 
-            //configurationService.CreateNewConfiguration(playerOne, gameplay, Color.Red);
-            //configurationService.CreateNewConfiguration(playerTwo, gameplay, Color.Black);
+            configurationService.CreateNewConfiguration(playerOne, gameplay, Color.Red);
+            configurationService.CreateNewConfiguration(playerTwo, gameplay, Color.Black);
 
-            //gameplayService.StartGameplay(gameplay);
+            gameplayService.StartGameplay(gameplay);
 
-            //playerService.AwardPoints(playerOne, gameplay, 10);
-            //playerService.AwardPoints(playerOne, gameplay, 5);
-            //playerService.AwardPoints(playerOne, gameplay, 2);
-            //playerService.SubstractPoints(playerOne, gameplay, 5);
-            //playerService.AwardPoints(playerTwo, gameplay, 1);
-            //playerService.AwardPoints(playerTwo, gameplay, 2);
-            //playerService.AwardPoints(playerTwo, gameplay, 3);
+            playerService.AwardPoints(playerOne, gameplay, 0);
+            playerService.AwardPoints(playerOne, gameplay, 5);
+            playerService.AwardPoints(playerOne, gameplay, 2);
+            playerService.SubstractPoints(playerOne, gameplay, 5);
+            playerService.AwardPoints(playerTwo, gameplay, 1);
+            playerService.AwardPoints(playerTwo, gameplay, 2);
+            playerService.AwardPoints(playerTwo, gameplay, 3);
 
-            //gameplayService.EndGemeplay(gameplay);
+            gameplayService.EndGemeplay(gameplay);
 
-            //playerService.DeletePlayer(playerThree);
+            playerService.DeletePlayer(playerThree);
 
             foreach (var player in gameplay.Players)
             {
